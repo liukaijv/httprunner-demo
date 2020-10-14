0cf068e1c1b1e6c96d5afe18b887bd7d0eb9aa34
ABC <89016230@qq.com>
2020-10-14 09:51:54 +0800

    多äå上传¼ 

--- debugtalk.py
+++ debugtalk.py
@@ -1,3 +1,4 @@
+import concurrent
 import logging
 import time
 from hashlib import md5
@@ -5,6 +6,11 @@ from json import dumps
 
 from httprunner import __version__
 from httprunner.response import ResponseObject
+import concurrent.futures
+
+import requests
+import uuid
+from typing import Dict, List
 
 
 def get_httprunner_version():
@@ -44,3 +50,48 @@ def set_sms_code_token(response: ResponseObject):
 def setup_hook():
     logging.info("setup_hook")
     pass
+
+
+def upload_file(file, token) -> Dict:
+    file_key = 'dev_test' + uuid.uuid4().hex
+    files = {
+        "file": open(file, 'rb'),
+    }
+    resp = requests.post('https://upload-z2.qiniup.com/', data={'token': token, 'key': file_key}, files=files)
+    return resp.json()
+
+
+def multi_upload(files: List, token) -> List:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+
+        upload_futures = {executor.submit(upload_file, f, token): f for f in files}
+
+        uploaded_files: List = []
+        for future in concurrent.futures.as_completed(upload_futures):
+            f = upload_futures[future]
+            try:
+                data = future.result()
+                if 'key' in data:
+                    uploaded_files.append(data['key'])
+            except Exception as ex:
+                print('上传' + f + '出错了:' + ex)
+        return uploaded_files
+
+
+def build_upload_data(file_keys: List, codes: List):
+    post_data = dict()
+    post_data['codeList'] = codes
+    post_data['infoList'] = [{'uri': uri, 'title': uri, 'type': 1} for uri in file_keys]
+    return post_data;
+
+
+def test_build_upload_img():
+    data = build_upload_data(['fafafaf'], [])
+    print(data)
+
+
+def test_multi_upload():
+    files = ['demo.png']
+    token = 'dmFonkLdUNhfdz9zFjdUOtrK5ktrMt86yic_iEYB:V6pCHKBaBl2q-OgpdnKOJDE-DQc=:eyJzY29wZSI6ImdlbmVyYWxyZXNvdXJjZXMiLCJkZWFkbGluZSI6MTYwMjU4NjY3Nn0='
+    ret = multi_upload(files, token)
+    print(ret)
